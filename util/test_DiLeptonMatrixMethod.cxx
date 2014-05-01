949b20c2a997141500a3de3ddffddeed50f9742d
Davide Gerbaudo <davide.gerbaudo@gmail.com>
2014-05-01 11:11:10 -0700

    add test validating the tool configuration
    Just a simple test with the latest stable matrix (Apr_10)
    showing also how to use the 1D and 2D parametrizations.

---
+++ util/test_DiLeptonMatrixMethod.cxx
@@ -0,0 +1,59 @@
+# include "SusyMatrixMethod/DiLeptonMatrixMethod.h"
+
+#include <iostream>
+#include <string>
+#include <cmath>
+
+/*
+  Test the matrix method with the recommended, most up-to-date matrix.
+  
+  davide.gerbaudo@gmail.com
+  April 2014
+*/
+
+using namespace std;
+namespace smm = SusyMatrixMethod;
+namespace sf = susy::fake;
+
+//----------------------------------------------------------
+bool testParametrization(string filename, smm::RATE_PARAM rp)
+{ 
+    bool success = false;
+    smm::DiLeptonMatrixMethod matrix;
+    if(matrix.configure(filename, rp, rp, rp, rp)) {
+        float gev2mev(1.0e3);
+        bool l0IsSig(true), l0IsEle(false);
+        bool l1IsSig(false), l1IsEle(true);
+        float l0Pt(30.0), l0Eta(+0.5);
+        float l1Pt(25.0), l1Eta(-0.5);
+        float metRel(20.0);
+        sf::Region region = sf::CR_SSInc1j;
+        smm::SYSTEMATIC sys = smm::SYS_NOM;
+        float weight = matrix.getTotalFake(l0IsSig, l0IsEle, l0Pt*gev2mev, l0Eta,
+                                           l1IsSig, l1IsEle, l1Pt*gev2mev, l1Eta,
+                                           region, metRel*gev2mev, sys);
+        cout<<"weight for"
+            <<" "<<sf::region2str(region)<<", "
+            <<" "<<smm::systematic_names[sys]
+            <<" : "<<weight
+            <<endl;
+        success = true;
+    }
+    return success;
+}
+//----------------------------------------------------------
+int main(int argc, char **argv)
+{
+
+  string inputFilename="data/FinalFakeHist_Apr_10.root";
+  size_t nFail=0;
+
+  cout<<endl<<" --- test 1D parametrization (pt)     ---"<<endl;
+  if(!testParametrization(inputFilename, smm::PT))
+      nFail++;
+  cout<<endl<<" --- test 2D parametrization (pt eta) ---"<<endl;
+  if(!testParametrization(inputFilename, smm::PT_ETA))
+      nFail++;
+  cout<<endl<<" --- Number of failures: "<<nFail<<" --- "<<endl;
+  return 0;
+}
