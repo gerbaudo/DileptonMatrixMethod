99f4f9ffb3fae74415f53f4edd540159fd458db9
Davide Gerbaudo <davide.gerbaudo@gmail.com>
2014-09-24 17:30:18 -0700

    add LinkDef; provides access from pyRoot

---
+++ Root/LinkDef.h
@@ -0,0 +1,24 @@
+#include <vector>
+
+#include "DileptonMatrixMethod/DileptonMatrixMethod.h"
+#include "DileptonMatrixMethod/Lepton.h"
+#include "DileptonMatrixMethod/Parametrization.h"
+#include "DileptonMatrixMethod/Systematic.h"            
+
+#ifdef __CINT__
+
+#pragma link off all globals;
+#pragma link off all classes;
+#pragma link off all functions;
+#pragma link C++ nestedclass;
+#pragma link C++ nestedtypedef;
+
+#pragma link C++ namespace susy+;
+#pragma link C++ namespace susy::fake+;
+
+#pragma link C++ class susy::fake::DileptonMatrixMethod+;
+#pragma link C++ class susy::fake::Lepton+;
+#pragma link C++ class susy::fake::Parametrization+;
+#pragma link C++ class susy::fake::Systematic+;
+
+#endif
