ae603eac4b2e20d8777fe846634869ddc2343a17
Davide Gerbaudo <davide.gerbaudo@gmail.com>
2014-12-02 20:49:48 -0800

    compare_matrices: print_summary_table

--- python/compare_matrices.py
+++ python/compare_matrices.py
@@ -3,6 +3,8 @@
 # Script to print out the rates from two matrix files
 #
 # Only the regions that are available in both files are considered.
+# If only one file is provided (twice), a summary table with all
+# values is printed out.
 #
 # davide.gerbaudo@gmail.com
 # Nov 2014
@@ -22,7 +24,9 @@ def main():
     filename2 = sys.argv[2]
     input_file1 = r.TFile.Open(filename1)
     input_file2 = r.TFile.Open(filename2)
-    if input_file1 and input_file2:
+    if (filename1 == filename2) and input_file1:
+        print_summary_table(input_file1)
+    elif input_file1 and input_file2:
         print "inputs:"
         print "1) {0}".format(input_file1.GetName())
         print "2) {0}".format(input_file2.GetName())
@@ -53,17 +57,18 @@ def getBinIndices(h) :
 def binContents(h) :
     return [h.GetBinContent(b) for b in getBinIndices(h)]
 
+def bc_format(bc, precision):
+    # return ('{:<'+str(precision+3)+'.'+str(precision)+'}').format(bc)
+    return ('{:.'+str(precision)+'}').format(bc).ljust(precision+len('0.0'))
+def ratio_format(a, b, precision):
+    width = precision+len('0.0')
+    return (('{:.'+str(precision)+'}').format(a/b) if b else '--').ljust(width)
+def formatted_histo_contents(h, precision=4):
+    return ' '.join(bc_format(bc, precision) for bc in binContents(h))
+def formatted_histos_ratio(h_num, h_den, precision=4):
+    return ' '.join(ratio_format(n, d, precision) for n,d in zip(binContents(h_num), binContents(h_den)))
+
 def print_histos_comparison(common_histos={'region':[]}, input_file1=None, input_file2=None):
-    def bc_format(bc, precision):
-        # return ('{:<'+str(precision+3)+'.'+str(precision)+'}').format(bc)
-        return ('{:.'+str(precision)+'}').format(bc).ljust(precision+len('0.0'))
-    def ratio_format(a, b, precision):
-        width = precision+len('0.0')
-        return (('{:.'+str(precision)+'}').format(a/b) if b else '--').ljust(width)
-    def formatted_histo_contents(h, precision=4):
-        return ' '.join(bc_format(bc, precision) for bc in binContents(h))
-    def formatted_histos_ratio(h_num, h_den, precision=4):
-        return ' '.join(ratio_format(n, d, precision) for n,d in zip(binContents(h_num), binContents(h_den)))
     for region, histos in common_histos.iteritems():
         print
         print '--- '+region+' ---'
@@ -75,6 +80,59 @@ def print_histos_comparison(common_histos={'region':[]}, input_file1=None, input
             print "1)   {0}".format(formatted_histo_contents(h1) if h1 else '--')
             print "2)   {0}".format(formatted_histo_contents(h2) if h2 else '--')
             print "2/1) {0}".format(formatted_histos_ratio(h2, h1))
+
+def print_summary_table(input_file):
+    "Loop over all histograms in the file, and summarized them in a table"
+    print "Summary for {0}".format(input_file.GetName())
+    histonames_per_region = classify_by_region(get_histonames(input_file))['complete']
+    regions = histonames_per_region.keys()
+    regions2d = [r for r in regions if '2d' in r]
+    regions1d = [r for r in regions if r not in regions2d]
+    histos_per_region = dict((r, [input_file.Get(hn) for hn in hns])
+                             for r, hns in histonames_per_region.iteritems())
+    # the line lenght is quite different for 1D and 2D histos, so print them separately
+    def is_2d(h) : return '2d' in h.GetName()
+    def is_1d(h) : return not is_2d(h)
+    histos1d = dict((r, filter(is_1d, histos_per_region[r])) for r in regions)
+    histos2d = dict((r, filter(is_2d, histos_per_region[r])) for r in regions)
+    if histos1d:
+        print "1D"
+        print_histos_contents_table(histos1d)
+    if histos2d:
+        print "2D"
+        print_histos_contents_table(histos2d)
+
+def print_histos_contents_table(histos_per_region={}):
+    "Print a summary with one line per region"
+    histonames_per_region = dict((r, [h.GetName() for h in histos])
+                                 for r, histos in histos_per_region.iteritems())
+    histoname_headers = sorted(list(set(h.replace('_'+r, '')
+                                        for r, hs in histonames_per_region.iteritems()
+                                        for h in hs)))
+    if not histoname_headers:
+        print "no histograms"
+        return
+    def h_matches_this_col(histo, col_header, region):
+        return col_header==histo.GetName().replace('_'+region, '')
+    # sort the histos in the dict according to the header
+    histos_per_region = dict((r, [[h for h in histos_per_region[r]
+                                   if h_matches_this_col(h, hh, r)][0]
+                                  for hh in histoname_headers])
+                             for r in histos_per_region.keys())
+    precision = 4
+    max_width_first_col = max(len(f) for f in histoname_headers)
+    max_width_other_col = max(len(f) for f in
+                              [formatted_histo_contents(h, precision)
+                               for hns in histos_per_region.values() for h in hns])
+    header = '  '.join(['region'.ljust(max_width_first_col)] +
+                       [h.ljust(max_width_other_col) for h in histoname_headers])
+    lines =  '\n'.join(['  '.join([r.ljust(max_width_first_col)] +
+                                  [formatted_histo_contents(h).ljust(max_width_other_col)
+                                   for h in histos_per_region[r]])
+                        for r in sorted(histos_per_region.keys())])
+    print header
+    print lines
+
 
 if __name__=='__main__':
     main()
